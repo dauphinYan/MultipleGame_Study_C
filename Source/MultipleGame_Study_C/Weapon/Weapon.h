85e497269b45428d5e47e7f3a591983019253a8f
dauphinYan <584485321@qq.com>
2024-11-28 22:06:38 +0800

    Create weapon component.

---
+++ Source/MultipleGame_Study_C/Weapon/Weapon.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+	EWS_Max UMETA(DisplayName = "DefaultMax")
+};
+
+UCLASS()
+class MULTIPLEGAME_STUDY_C_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+
+	//void ShowPickupWidget(bool bShow);
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 重叠开始时调用
+	//UFUNCTION()
+	//void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//// 重叠结束时调用
+	//UFUNCTION()
+	//void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	class USphereComponent* Sphere;
+
+	//UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	//USkeletalMeshComponent* WeaponMesh;
+};
